e24d3a29ef52f1694548f5856d9f8be22d80d888
Irene Patsoura <irene.patsoura@gmail.com>
2022-10-28 14:09:11 +0300

    Add new function to unittest

--- pyECSS/tests/test_SkinnedMesh.py
+++ pyECSS/tests/test_SkinnedMesh.py
@@ -11,18 +11,26 @@ class TestComponent(unittest.TestCase):
     def setUp(self):
         
         self.skinnedMeshComponent = Skinned_mesh(filename='/Users/mlbeb/Desktop/boo/internship/py_code/pyECSSTree/pyECSS/examples/astroBoy_walk.dae', file_type='dae')
+
        
-    def file_exists():
+    def test_file_exists(self):
         assert(os.path.exists('/Users/mlbeb/Desktop/boo/internship/py_code/pyECSSTree/pyECSS/examples/')==True)
         
-    print("TestComponent:test_animation() START")
+    print("TestComponent:test_animation() START")    
      
     def test_animation(self):
         a = self.skinnedMeshComponent
         self.assertNotEqual(a, None)
-        
-        
+    
     print("TestComponent:test_animation() END")
+
+        
+    def test_vertices(self):
+        a = self.skinnedMeshComponent
+        for i in range(len(a.v)):
+            assert(len(a.v[i])==3)
+
+         
         
 # run the test
 if __name__ == "__main__":
