9b9967e3891d79df3ee2880964f850dc68dcd177
Irene Patsoura <irene.patsoura@gmail.com>
2022-10-28 12:30:37 +0300

    Check in unittest if file exists

--- pyECSS/tests/test_SkinnedMesh.py
+++ pyECSS/tests/test_SkinnedMesh.py
@@ -3,6 +3,7 @@ import unittest
 from pyECSS.examples.System_skinning import System_skinning
 from pyECSS.examples.skinned_mesh import Skinned_mesh
 from pyECSS.examples.gate_module import *
+import os.path
 
 class TestComponent(unittest.TestCase):
     
@@ -11,6 +12,9 @@ class TestComponent(unittest.TestCase):
         
         self.skinnedMeshComponent = Skinned_mesh(filename='/Users/mlbeb/Desktop/boo/internship/py_code/pyECSSTree/pyECSS/examples/astroBoy_walk.dae', file_type='dae')
        
+    def file_exists():
+        assert(os.path.exists('/Users/mlbeb/Desktop/boo/internship/py_code/pyECSSTree/pyECSS/examples/')==True)
+        
     print("TestComponent:test_animation() START")
      
     def test_animation(self):
